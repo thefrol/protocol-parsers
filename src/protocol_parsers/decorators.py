2fdac5f166be7189103391c35bc00d70f46b1c38
Дмитрий Фроленко <orangefrol@gmail.com>
2023-05-29 15:16:21 +0300

    a new decorator converts to int

--- src/protocol_parsers/decorators.py
+++ src/protocol_parsers/decorators.py
@@ -2,3 +2,12 @@ def trim(f):
     def callee(*args,**kwargs):
         return f(*args,**kwargs).strip()
     return callee
+
+def to_int(f):
+    def callee(*args,**kwargs):
+        try:
+            return int(f(*args,**kwargs))
+        except Exception as e:
+            print('cant convert to int {f.__name__}')
+            return None
+    return callee
