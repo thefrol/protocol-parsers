c30cde385bd03d9df52eb2b0e274a2192a4d0696
Дмитрий Фроленко <orangefrol@gmail.com>
2023-06-06 21:45:05 +0300

    to_int output fix

--- src/protocol_parsers/decorators.py
+++ src/protocol_parsers/decorators.py
@@ -8,6 +8,6 @@ def to_int(f):
         try:
             return int(f(*args,**kwargs))
         except Exception as e:
-            print(f'cant convert to int {f.__name__}')
+            print(f'cant convert to int — {f.__name__}')
             return None
     return callee
