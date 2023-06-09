4305a90b621280840f633d74eda4b9e0d4e5c2ca
Дмитрий Фроленко <orangefrol@gmail.com>
2023-06-09 22:24:46 +0300

    stores to check

--- scripts/store_check.py
+++ scripts/store_check.py
@@ -1,21 +1,44 @@
-from protocol_parsers.decorators import store_,store_first_output
+from protocol_parsers.decorators import store_first_output, new_store_first_output, fast_store_first_output, replace_store_first_output,id_store_first_output
 import time
 
 class Cl:
     def __init__(self, val):
          self.val=val
+         self._b=3
+         self._c=3
+         self._d=3
+         self._u=3
+         self._g=3
+         self._g22=3
     @property
     @store_first_output
     def from_store(self):
-        print('evaluating')
         return self.val
     @property
     def no_store(self):
         return self.val
+    @property
+    @new_store_first_output
+    def new_store(self):
+        return self.val
+    
+    @property
+    @fast_store_first_output
+    def fast_store(self):
+        return self.val
+    @property
+    @replace_store_first_output
+    def replace_store(self):
+        return self.val
+    @property
+    @id_store_first_output
+    def id_store(self):
+        return self.val
+    
     
 def run_1000_times(func):
     start=time.time()
-    for i in range(1000):
+    for i in range(1000000):
         func()
     total=time.time()-start
     return total
@@ -24,13 +47,17 @@ def run_1000_times(func):
     
 c=Cl(1)
 b=Cl(2)
+c.replace_store
+c.fast_store
+c.new_store
+c.from_store
+c.id_store
 
-print(c.from_store)
-print(c.from_store)
-print(b.from_store)
-print(c.from_store)
-print(b.from_store)
-print(b.from_store)
 
-print(f'from store 1000 times = {run_1000_times(lambda: c.from_store)}')
 print(f'no store 1000 times = {run_1000_times(lambda: c.no_store)}')
+print(f'new store 1000 times = {run_1000_times(lambda: c.new_store)}')
+print(f'fast store 1000 times = {run_1000_times(lambda: c.fast_store)}')
+print(f'from store 1000 times = {run_1000_times(lambda: c.from_store)}')
+print(f'replace store 1000 times = {run_1000_times(lambda: c.replace_store)}')
+print(f'id store 1000 times = {run_1000_times(lambda: c.id_store)}')
+
--- src/protocol_parsers/decorators.py
+++ src/protocol_parsers/decorators.py
@@ -46,6 +46,66 @@ def store_first_output(f): #TODO add with_name
             return value_to_save
     return callee
 
+def new_store_first_output(f): #TODO add with_name
+    """some sort of lazy initializer
+    stores the first value or the called method,
+    sloud me used for propetries only"""
+    def callee(self, *args,**kwargs):
+        if f.__name__ in self.__dict__.get('__output_store__',{}):
+            return self.__dict__['__output_store__'][f.__name__]
+        else:
+            if self.__dict__.get('__output_store__') is None: # create output store if needed
+                self.__dict__['__output_store__']={}
+            value_to_save=f(self,*args,**kwargs)
+            self.__dict__['__output_store__'][f.__name__]=value_to_save
+            return value_to_save
+    return callee
+
+#fastest
+def fast_store_first_output(f): #TODO add with_name
+    """some sort of lazy initializer
+    stores the first value or the called method,
+    sloud me used for propetries only"""
+    def callee(self, *args,**kwargs):
+        store_name='__lazy__'+f.__name__
+        if store_name in self.__dict__:
+            return self.__dict__[store_name]
+        else:
+            value_to_save=f(self,*args,**kwargs)
+            self.__dict__[store_name]=value_to_save
+            return value_to_save
+    return callee
+
+#winner!!!!
+def id_store_first_output(f): #TODO add with_name
+    """some sort of lazy initializer
+    stores the first value or the called method,
+    sloud me used for propetries only"""
+    __store={}
+    def callee(self, *args,**kwargs):
+        nonlocal __store
+        id_=id(self)
+        if id_ in __store:
+            return __store[id_]
+        else:
+            value_to_save=f(self,*args,**kwargs)
+            __store[id_]=value_to_save
+            return value_to_save
+    return callee
+
+#not working
+def replace_store_first_output(f): #TODO add with_name
+    """some sort of lazy initializer
+    stores the first value or the called method,
+    sloud me used for propetries only"""
+    def callee(self, *args,**kwargs):
+            value_to_save=f(self,*args,**kwargs)
+            #print('stored')
+            self.__dict__[f.__name__]=value_to_save
+            return value_to_save
+    return callee
+
+
 # def store_(f):
 #     _local_value=None
 #     _is_stored=False # in case _local_value is None and we may check if we called the function already
