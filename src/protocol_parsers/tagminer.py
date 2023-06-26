a0643e7d829feebb953758822d275bae3f0e7f65
Дмитрий Фроленко <orangefrol@gmail.com>
2023-06-26 21:03:44 +0300

    tagminer refactor

---
+++ src/protocol_parsers/tagminer.py
@@ -0,0 +1,31 @@
+from typing import Callable
+
+class TagMiner:
+    """a basic class for working with tags, a wrapper for bs4 parser"""
+    def __init__(self, html):
+        self._html=html
+    def _find_tag(self, tag, class_):
+        ##TODO should return some metaclass, or we should have an error wrapper for this when getting text
+        return self._html.find(tag,{'class':class_})
+    def _find_all_tags(self, tag, class_):
+        return self._html.find_all(tag,{'class':class_})
+    def tag_text(self):
+        return self._html.text
+    def find_in_parents(self, comparator:Callable, search_depth=10): 
+        """Function for searching in parent tags
+        ex. if one of parent tags has a class 'bar'
+        find_in_parent_tags(lambda tag: 'bar' in tag['class'])"""
+        current_tag=self._html.parent
+        for i in range(search_depth):
+            if comparator(current_tag):
+                return True
+            current_tag=current_tag.parent
+        return False
+    def get_param(self, attribute_name, default=None):
+        """safely returns html tag attribute value and fallbacks to default if param not found
+        self.get('href', 'no link')
+        #/player/12342"""
+        if attribute_name in self._html.attrs:
+            return self._html.attrs[attribute_name]
+        else:
+            return default
--- src/protocol_parsers/yfl/__init__.py
+++ src/protocol_parsers/yfl/__init__.py
@@ -6,6 +6,9 @@ from ..decorators import trim, to_int, to_int_or_none
 import re
 from ..regex import Regex, Regexes2
 
+from ..tagminer import TagMiner
+from .player_page import PlayerPage
+
 @to_int_or_none
 def get_player_id(player_relative_url:str):
     return Regex(pattern=r'/player/(?P<player_id>\d+)',
@@ -13,35 +16,7 @@ def get_player_id(player_relative_url:str):
     
 
 
-class TagMiner:
-    """a basic class for working with tags, a wrapper for bs4 parser"""
-    def __init__(self, html):
-        self._html=html
-    def _find_tag(self, tag, class_):
-        ##TODO should return some metaclass, or we should have an error wrapper for this when getting text
-        return self._html.find(tag,{'class':class_})
-    def _find_all_tags(self, tag, class_):
-        return self._html.find_all(tag,{'class':class_})
-    def tag_text(self):
-        return self._html.text
-    def find_in_parents(self, comparator:Callable, search_depth=10): 
-        """Function for searching in parent tags
-        ex. if one of parent tags has a class 'bar'
-        find_in_parent_tags(lambda tag: 'bar' in tag['class'])"""
-        current_tag=self._html.parent
-        for i in range(search_depth):
-            if comparator(current_tag):
-                return True
-            current_tag=current_tag.parent
-        return False
-    def get_param(self, attribute_name, default=None):
-        """safely returns html tag attribute value and fallbacks to default if param not found
-        self.get('href', 'no link')
-        #/player/12342"""
-        if attribute_name in self._html.attrs:
-            return self._html.attrs[attribute_name]
-        else:
-            return default
+
 
 
 class MatchProtocolTabPlayer(TagMiner):
