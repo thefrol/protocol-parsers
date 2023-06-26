e4519385c92884a68055ece10f2276f78c370e7d
Дмитрий Фроленко <orangefrol@gmail.com>
2023-06-26 21:04:28 +0300

    webparser refactor

--- src/protocol_parsers/yfl_parser.py
+++ src/protocol_parsers/yfl_parser.py
@@ -1,38 +1,7 @@
-import re
-import requests
-import json
-from itertools import chain
 from datetime import datetime
-from bs4 import BeautifulSoup
-from abc import abstractmethod
 
-
+from .webparser import WebParser
 from .yfl import MatchPage,Team
-
-
-class WebParser:
-    """a class that gets a link and returns a json with needed data"""
-    url_pattern=r'https://mosff.ru/match/\d+'
-    page_class=None
-    def __init__(self, url:str):
-        if not re.fullmatch(self.url_pattern,url):
-            print(f'seems like {url} not fits url pattern{self.url_pattern}')
-        
-        page=requests.get(url) 
-
-        if page.status_code != 200:
-            raise ConnectionError('page not retrieved')
-        
-        self._html=BeautifulSoup(page.text, 'html.parser')
-        self.page=self.page_class(self._html)
-    
-    @abstractmethod
-    def to_rbdata(self):
-        pass
-    
-    def to_json(self)->str:
-        '''returns a json string formatted in style of rbdata'''
-        return json.dumps(self.to_rbdata(),ensure_ascii=False)
 
 
 class YflParser(WebParser):
