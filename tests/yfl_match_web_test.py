7b3a83c26888b86a6657561ee8c0e8ea4683ad23
Дмитрий Фроленко <orangefrol@gmail.com>
2023-07-09 19:56:33 +0300

    events test

--- tests/yfl_match_web_test.py
+++ tests/yfl_match_web_test.py
@@ -1,7 +1,8 @@
 import unittest
+from typing import Callable
 
 from protocol_parsers import YflParser
-from protocol_parsers.yfl  import MatchPage
+from protocol_parsers.yfl  import MatchPage, Team
 from datetime import datetime
 #test time in, time out, time played
 # def lineup_status(lineup)
@@ -14,6 +15,13 @@ from datetime import datetime
 # test teams id
 # test players count
 # test autogoals
+
+def count_players(team:Team, func:Callable):
+    count=0
+    for player in team.players:
+        if func(player):
+            count=count+1
+    return count
 
 class BasicMatchTest(unittest.TestCase):
     @classmethod
@@ -48,7 +56,54 @@ class BasicMatchTest(unittest.TestCase):
         self.assertIs(home_team._parent_match,self.page,'home team has wrong parent match')
         self.assertIs(guest_team._parent_match,self.page,'guest team has wrong parent match')
 
+    def test_main_and_bench(self):
+        """counts players at main, subbed players and bench players"""
+        home_team_start_players_count=count_players(
+            self.page.home_team,
+            lambda p: p.time_in==0
+        )
+        guest_team_start_players_count=count_players(
+            self.page.guest_team,
+            lambda p: p.time_in==0
+        )
 
+        self.assertEqual(home_team_start_players_count,11,'must be 11 players at start')
+        self.assertEqual(guest_team_start_players_count,11,'must be 11 players at start')
+
+        home_team_sub_players_count=count_players(
+            self.page.home_team,
+            lambda p: p.time_in is not None and p.time_in>0
+        )
+
+        guest_team_sub_players_count=count_players(
+            self.page.guest_team,
+            lambda p: p.time_in is not None and p.time_in>0
+        )
+
+        self.assertEqual(home_team_sub_players_count,5,'must be 5 subbed players at Rubin')
+        self.assertEqual(guest_team_sub_players_count,4,'must be 4 subbed players at CSKA')
+
+        home_team_bench_players_count=count_players(
+            self.page.home_team,
+            lambda p: p.time_in is None
+        )
+
+        guest_team_bench_players_count=count_players(
+            self.page.guest_team,
+            lambda p: p.time_in is None
+        )
+
+        self.assertEqual(home_team_bench_players_count,3,'must be 3 bench players at Rubin')
+        self.assertEqual(guest_team_bench_players_count,4,'must be 4 bench players at CSKA')
+    
+    def test_events(self):
+        self.assertEqual(self.page.events.yellow_cards, 7, 'total yellow cards is 7')
+        self.assertEqual(self.page.home_team.events.yellow_cards, 2, 'home team yellow cards is 2')
+        self.assertEqual(self.page.guest_team.events.yellow_cards, 4, 'guest team yellow cards is 4, and one for trainer not counted') # without a trainer
+
+        self.assertEqual(self.page.events.goals,5,'totally 5 goals')
+        self.assertEqual(self.page.home_team.events.goals,2) # TODO add to self check if events goals are not equal to toal goals from promo
+        self.assertEqual(self.page.guest_team.events.goals,3)
 
 
 if __name__ == '__main__':
