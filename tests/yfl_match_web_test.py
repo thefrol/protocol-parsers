ca1aa32ad5aba5a567079130cc56c03d51734fd3
Дмитрий Фроленко <orangefrol@gmail.com>
2023-07-09 10:50:47 +0300

    initial brach

---
+++ tests/yfl_match_web_test.py
@@ -0,0 +1,19 @@
+import unittest
+
+#test time in, time out, time played
+# def lineup_status(lineup)
+#    return 'bench' if lineup['time_played'] == 0
+#    lineup['time_in'] == @ ? 'lineup' : 'sub'
+# end
+#
+# test red cards and time played
+# test players id
+# test teams id
+# test players count
+# test autogoals
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
