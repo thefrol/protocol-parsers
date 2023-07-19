f3751d6703a1460fbf922ce2bdda1004f74c997e
Дмитрий Фроленко <orangefrol@gmail.com>
2023-07-19 23:22:00 +0300

    new case

--- tests/exports/mosff_test.py
+++ tests/exports/mosff_test.py
@@ -1,5 +1,5 @@
 import unittest
-from exports.base import load_dicts, is_subset, find_files
+from exports.base import load_dicts, is_subset, find_files, unequal_params
 
 #TODO test for errors on creation
 
@@ -7,15 +7,15 @@ class BasicTest(unittest.TestCase):
     def test_protocols(self):
         for file in find_files('mosff_match'):
             data,file_data=load_dicts(file.name)
-            self.assertTrue(is_subset(data, file_data), f'error parsing saved file {file.name}')
+            self.assertTrue(is_subset(data, file_data), f'error parsing saved file {file.name} \n unequal params {unequal_params(data, file_data)}')
     def test_players(self):
         for file in find_files('mosff_player'):
             data,file_data=load_dicts(file.name)
-            self.assertTrue(is_subset(data, file_data), f'error parsing saved file {file.name}')
+            self.assertTrue(is_subset(data, file_data), f'error parsing saved file {file.name} \n unequal params {unequal_params(data, file_data)}')
     def test_teams(self):
         for file in find_files('mosff_team'):
             data,file_data=load_dicts(file.name)
-            self.assertTrue(is_subset(data, file_data), f'error parsing saved file {file.name}')
+            self.assertTrue(is_subset(data, file_data), f'error parsing saved file {file.name} \n unequal params {unequal_params(data, file_data)}')
 
 
 if __name__ == '__main__':
