fde7c6ba852e42aa260e74ac0b1ab56a0fd9622d
Дмитрий Фроленко <orangefrol@gmail.com>
2023-07-19 23:27:24 +0300

    stuff

---
+++ scripts/mosff_blocks.py
@@ -0,0 +1,17 @@
+from protocol_parsers import MosffParser
+from protocol_parsers.mosff.player_block import PlayerBlock,PlayerBlockList
+from pprint import pprint
+
+
+#URL='https://mosff.ru/match/34549'
+URL='https://mosff.ru/match/38876' #CUP
+#URL='https://mosff.ru/match/38860' #CUP 2009
+#URL='https://mosff.ru/match/34540' #no minute for autogoals, add to tests
+
+def home_players(block:PlayerBlock):
+    return block.is_home_team and block.is_players
+
+p=MosffParser(URL)
+b=p.page.player_blocks.home_players
+
+pprint(b)
