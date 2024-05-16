06ff5e611879448026d0020387aac199ced80328
Dima <orangefrol@gmail.com>
2024-05-16 11:46:34 +0300

    fix: уточнить важный момент по датам матчей в ридми

--- scripts/mosff_protocol.py
+++ scripts/mosff_protocol.py
@@ -5,7 +5,8 @@ from pprint import pprint
 #URL='https://mosff.ru/match/34549'
 #URL='https://mosff.ru/match/38876' #CUP
 #URL='https://mosff.ru/match/38860' #CUP  2009
-URL='https://mosff.ru/match/34540' #no minute for autogoals, add to tests
+#URL='https://mosff.ru/match/34540' #no minute for autogoals, add to tests
+URL = "https://mosff.ru/match/52837"
 
 
 d=MosffParser(URL)
